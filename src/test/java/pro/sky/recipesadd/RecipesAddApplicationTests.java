3785e5e17ab5df88aea11da8fb91d826684b6aa4
Evgeniy Zinchenko <JjjackVat@gmail.com>
2023-01-09 08:15:23 +0300

    Initial commit

---
+++ src/test/java/pro/sky/recipesadd/RecipesAddApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.recipesadd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesAddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
